33c70dc3fe28e178289cd39a2a8249fae0bfd4ed
pnrbrk41 <113897255+pnrbrk41@users.noreply.github.com>
2022-09-23 22:10:08 +0300

    son duzenleme
    son düzenleme

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+    
+
+	namespace If-Else-Yap-s-
+	{
+
+
+   class Program
+   
+   {
+      static void Main (string[] args )
+      {
+         int time=DateTime.Now.Hour;
+
+         if(time >=6 && time<11)
+             Console.WriteLine( "Günaydın");
+
+         else if(time<=18)
+            Console.WriteLine("İyi günler");
+
+        else
+            Console.WriteLine("iyi geceler");
+
+            string sonuc= time<=18 ? "iyi günler" : "iyi geceler";
+
+
+
+      }
+
+   }
+}
